66068a1bae776aac4a99b0f3f665d8310c0f1f38
詹庆祥 <zqx1028_love@126.com>
2016-08-23 14:45:59 +0800

    update

--- views/layout.jsx
+++ views/layout.jsx
@@ -3,14 +3,19 @@ let Layout = React.createClass({
     render: function(){
         return (
             <html>
-            <head>
-                <title>{this.props.title}</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <link href="/stylesheets/style.css" rel="stylesheet"/>
-            </head>
-            <body>
-                {this.props.children}
-            </body>
+                <head>
+                    <title>{this.props.title}</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <link href="/stylesheets/style.css" rel="stylesheet"/>
+                </head>
+                <body>
+                    <header>
+                        <ul>
+                            <li><a href="/index">首页</a></li>
+                        </ul>
+                    </header>
+                    <div>{this.props.children}</div>
+                </body>
             </html>
         )
     }
